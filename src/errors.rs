c0c66e62543aad1094a8794552090e83082416a1
Frank Denis <github@pureftpd.org>
2019-09-22 02:30:05 +0200

    We don't define any custom error types

--- src/errors.rs
+++ src/errors.rs
@@ -1,16 +1,1 @@
 pub use failure::{bail, ensure, Error};
-use privdrop::PrivDropError;
-use std::io;
-use std::net::AddrParseError;
-
-#[derive(Debug, Fail)]
-pub enum ProxyError {
-    #[fail(display = "Internal error: [{}]", _0)]
-    InternalError(String),
-    #[fail(display = "I/O error: [{}]", _0)]
-    Io(#[cause] io::Error),
-    #[fail(display = "Unable to parse address: [{}]", _0)]
-    AddrParseError(#[cause] AddrParseError),
-    #[fail(display = "Privilege drop error: [{}]", _0)]
-    PrivDrop(#[cause] PrivDropError),
-}
