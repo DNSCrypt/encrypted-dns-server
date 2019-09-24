f343802fd07a61f7a799228ae4a0b4ca3c982c5c
Frank Denis <github@pureftpd.org>
2019-09-24 19:55:51 +0200

    Revert "Nits"
    This reverts commit 0f63c5e5942ae4e9eb4719b38006081f1c200d23.

--- src/main.rs
+++ src/main.rs
@@ -527,11 +527,15 @@ fn main() -> Result<(), Error> {
         updater.update();
     }
     runtime.spawn(updater.run());
-    match runtime.block_on(start(globals, runtime.clone())) {
-        Ok(()) => Ok(()),
-        Err(e) => {
-            error!("Unable to start the service: [{}]", e);
-            std::process::exit(1);
-        }
-    }
+    runtime.spawn(
+        start(globals, runtime.clone())
+            .map_err(|e| {
+                error!("Unable to start the service: [{}]", e);
+                std::process::exit(1);
+            })
+            .map(|_| ()),
+    );
+    runtime.block_on(future::pending::<()>());
+
+    Ok(())
 }
