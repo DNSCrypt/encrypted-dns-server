ed72e0e064979a9416d57b757b00e8d90fbbd57a
Frank Denis <github@pureftpd.org>
2020-11-02 22:17:46 +0100

    Don't bail out if only metrics are enabled

--- src/main.rs
+++ src/main.rs
@@ -485,7 +485,7 @@ fn bind_listeners(
 }
 
 fn privdrop(config: &Config) -> Result<(), Error> {
-    if config.metrics.is_some() {
+    if config.daemonize && config.metrics.is_some() {
         bail!("Metrics are incompatible with daemonization - set 'daemonize = false' in the configuration file if you need metrics.");
     }
     let mut pd = PrivDrop::default();
